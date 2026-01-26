15d2ff67fa14af21c0258a76544de6688187f0ad
Charann05 <charannpoojary@gmail.com>
2026-01-26 12:57:59 +0530

    Add ResultActivity to display the final score

--- Quiz App/app/src/main/java/com/cp/quizapp/ui/result/ResultActivity.kt
+++ Quiz App/app/src/main/java/com/cp/quizapp/ui/result/ResultActivity.kt
@@ -1,21 +1,31 @@
 package com.cp.quizapp.ui.result
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.cp.quizapp.MainActivity
 import com.cp.quizapp.R
 
 class ResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvScore = findViewById<TextView>(R.id.tvScore)
+        val btnHome = findViewById<Button>(R.id.btnHome)
+
+        val score = intent.getIntExtra("SCORE", 0)
+        val total = 10
+
+        tvScore.text = "Your Score: $score / $total"
+
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
